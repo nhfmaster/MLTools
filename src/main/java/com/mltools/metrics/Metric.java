2fccaa0b2e868b1a45344b336bd663a52de5b780
nhfmaster <794827982@qq.com>
2017-12-08 14:14:55 +0800

    finish regression metric

---
+++ src/main/java/com/mltools/metrics/Metric.java
@@ -0,0 +1,95 @@
+package com.mltools.metrics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by nhfmaster on 2017/12/8.
+ */
+public class Metric {
+    /**
+     * calculate data average(mean)
+     *
+     * @param dataList data
+     * @return average(mean) value
+     */
+    public static double calAverage(List<Double> dataList) {
+        double sum = 0.0;
+        for (double data : dataList)
+            sum += data;
+        return sum / dataList.size();
+    }
+
+    /**
+     * calculate data variance
+     *
+     * @param dataList data
+     * @return variance value
+     */
+    public static double calVariance(List<Double> dataList) {
+        double average = calAverage(dataList);
+        double variance = 0.0;
+        for (double data : dataList)
+            variance += Math.pow(data - average, 2);
+        return variance / dataList.size();
+    }
+
+    /**
+     * calculate loge(1+data)
+     *
+     * @param dataList data
+     * @return loge calculate list
+     */
+    public static List<Double> getLogEList(List<Double> dataList) {
+        List<Double> logEList = new ArrayList<Double>();
+        for (double data : dataList)
+            logEList.add(Math.log(1.0 + data));
+        return logEList;
+    }
+
+    /**
+     * calculate true value minus predicted value
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     * @return list of true value minus predicted value
+     */
+    public static List<Double> getMinusList(List<Double> predictList, List<Double> trueList) {
+        List<Double> minusList = new ArrayList<Double>();
+        for (int i = 0; i < trueList.size(); i++)
+            minusList.add(trueList.get(i) - predictList.get(i));
+        return minusList;
+    }
+
+    /**
+     * calculate abs of true value minus predicted value
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     * @return list of abs of true value minus predicted value
+     */
+    public static List<Double> getMinusAbsList(List<Double> predictList, List<Double> trueList) {
+        List<Double> minusList = new ArrayList<Double>();
+        for (int i = 0; i < trueList.size(); i++)
+            minusList.add(Math.abs(trueList.get(i) - predictList.get(i)));
+        return minusList;
+    }
+
+    /**
+     * calculate median
+     *
+     * @param list data
+     * @return median
+     */
+    public static double getMedian(List<Double> list) {
+        if (list.size() == 0)
+            return 0;
+        Collections.sort(list);
+        int size = list.size();
+        if (size % 2 == 0)
+            return (list.get(size / 2 - 1) + list.get(size / 2)) / 2;
+        else
+            return list.get((size + 1) / 2 - 1);
+    }
+}
--- src/main/java/com/mltools/metrics/RegressionMetric.java
+++ src/main/java/com/mltools/metrics/RegressionMetric.java
@@ -1,7 +1,96 @@
 package com.mltools.metrics;
 
+import java.util.List;
+
 /**
  * Created by nhfmaster on 2017/12/5.
  */
 public class RegressionMetric {
+    /**
+     * calculate explained variance
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     */
+    public static void calExplainedVariance(List<Double> predictList, List<Double> trueList) {
+        double trueVariance = Metric.calVariance(trueList);
+        List<Double> minusList = Metric.getMinusList(predictList, trueList);
+        double minusVariance = Metric.calVariance(minusList);
+        double explainedVariance = 1 - minusVariance / trueVariance;
+        System.out.println("Explained variance: " + explainedVariance);
+    }
+
+    /**
+     * calculate Mean absolute error
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     */
+    public static void calMeanAbsoluteError(List<Double> predictList, List<Double> trueList) {
+        double error = 0.0;
+        for (int i = 0; i < trueList.size(); i++)
+            error += Math.abs(trueList.get(i) - predictList.get(i));
+        error = error / trueList.size();
+        System.out.println("Mean absolute error: " + error);
+    }
+
+    /**
+     * calculate Mean squared error
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     */
+    public static void calMeanSquaredError(List<Double> predictList, List<Double> trueList) {
+        double error = 0.0;
+        for (int i = 0; i < trueList.size(); i++)
+            error += Math.pow(trueList.get(i) - predictList.get(i), 2);
+        error = error / trueList.size();
+        System.out.println("Mean squared error: " + error);
+    }
+
+    /**
+     * calculate Mean squared logarithmic error
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     */
+    public static void calMeanSquaredLogarithmicError(List<Double> predictList, List<Double> trueList) {
+        List<Double> predictLogEList = Metric.getLogEList(predictList);
+        List<Double> trueLogEList = Metric.getLogEList(trueList);
+        double error = 0.0;
+        for (int i = 0; i < trueLogEList.size(); i++)
+            error += Math.pow(trueLogEList.get(i) - predictLogEList.get(i), 2);
+        error = error / trueLogEList.size();
+        System.out.println("Mean squared logarithmic error: " + error);
+    }
+
+    /**
+     * calculate Median absolute error
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     */
+    public static void calMedianAbsoluteError(List<Double> predictList, List<Double> trueList) {
+        List<Double> minusAbsList = Metric.getMinusAbsList(predictList, trueList);
+        double median = Metric.getMedian(minusAbsList);
+        System.out.println("Median absolute error: " + median);
+    }
+
+    /**
+     * calculate R2 score
+     *
+     * @param predictList predicted value
+     * @param trueList    true value
+     */
+    public static void calR2Score(List<Double> predictList, List<Double> trueList) {
+        double trueAverage = Metric.calAverage(trueList);
+        double numerator = 0.0;
+        double denominator = 0.0;
+        for (int i = 0; i < trueList.size(); i++) {
+            denominator += Math.pow(trueList.get(i) - trueAverage, 2);
+            numerator += Math.pow(trueList.get(i) - predictList.get(i), 2);
+        }
+        double r2Score = 1 - numerator / denominator;
+        System.out.println("R2 Score: " + r2Score);
+    }
 }
